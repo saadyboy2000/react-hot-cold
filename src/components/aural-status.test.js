5690fda7cdf60aab8714c153b5e2ce6d31cad4dc
saaderana <saaderana@gmail.com>
2017-12-07 08:50:32 -0800

    added travis

--- src/components/aural-status.test.js
+++ src/components/aural-status.test.js
@@ -4,14 +4,14 @@ import { shallow } from 'enzyme';
 import AuralStatus from './aural-status';
 
 describe('<AuralStatus />', () => {
-	it('Renders without crashing', () => {
-		shallow(<AuralStatus />);
-	});
+  it('Renders without crashing', () => {
+    shallow(<AuralStatus />);
+  });
 
-	it('Renders an aural status update', () => {
-		let TEST_STATUS = 'You are listening to a game!';
-
-		let wrapper = shallow(<AuralStatus auralStatus={TEST_STATUS}/>);
-		expect(wrapper.contains(TEST_STATUS)).toEqual(true);
-	});
+  it('Renders an aural status update', () => {
+    let TEST_STATUS = 'You are listening to a game!';
+    
+    let wrapper = shallow(<AuralStatus auralStatus={TEST_STATUS} />);
+    expect(wrapper.contains(TEST_STATUS)).toEqual(true);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -10,3 +10,7 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+//"site_id":"0b726e89-e9db-4fdb-a922-6909d076dd84"}
+//"access_token":"3b895f836a2fded185c0abe8dba1fa119e31e8c5bbb6230bd250ce4b3979ed07"
+//token: 1604eaf83a10b2e03fe2e37705fe7c5de308969f2412e4c9982734ca6aa086da
