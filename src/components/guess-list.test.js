8fa014b0ca23df9311770cb419e97ad8183dc26f
saaderana <saaderana@gmail.com>
2017-12-07 08:59:46 -0800

    changed guess-list.test.js

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -3,18 +3,18 @@ import { shallow } from 'enzyme';
 
 import GuessList from './guess-list';
 
-describe('<GuessList />' () => {
-	it('Renders without crashing', () => {
-		shallow(<GuessList guesses = {[]} />);
-	});
+describe('<GuessList />', () => {
+  it('Renders without crashing', () => {
+    shallow(<GuessList guesses={[]} />);
+  });
 
-	it('Renders a list of guesses', () => {
-		const values = [10,24,52];
-		const wrapper = shallow(<GuessList guesses= {values} />);
-		const items = wrapper.find('li');
-		expect(items.length).toEqual(values.length);
-		values.forEach((value, index) => {
-			expect(items.at(index).text()).toEqual(value.toString());
-		});
-	});
+  it('Renders a list of guesses', () => {
+    const values = [10, 24, 52];
+    const wrapper = shallow(<GuessList guesses={values} />);
+    const items = wrapper.find('li');
+    expect(items.length).toEqual(values.length);
+    values.forEach((value, index) => {
+      expect(items.at(index).text()).toEqual(value.toString());
+    });
+  });
 });
