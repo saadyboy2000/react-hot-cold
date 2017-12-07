c12b58cfa691363b8413c8ed3417071126c7038a
saaderana <saaderana@gmail.com>
2017-12-07 09:07:16 -0800

    fixed some errors

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -4,27 +4,27 @@ import { shallow } from 'enzyme';
 import TopNav from './top-nav';
 
 describe('<TopNav />', () => {
-	it('Renders without crashing', () => {
-		shallow(<TopNav />);
-	});
+  it('Renders without crashing', () => {
+    shallow(<TopNav />);
+  });
 
-	it('Should call onNewGame when new game is clicked', () => {
-		const callback = jest.fn();
-		const wrapper = shallow(<TopNav onRestartGame={callback} />);
-		const link = wrapper.find('.new');
-		link.simulate('click', {
-			preventDefault(){}
-		});
-		expect(callback).toHaveBeenCalled();
-	});
+  it('Should call onNewGame when new game is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    const link = wrapper.find('.new');
+    link.simulate('click', {
+      preventDefault() {}
+    });
+    expect(callback).toHaveBeenCalled();
+  });
 
-	it('Should call onGenerateAuralUpdate when state-of-game link is clicked', () => {
-		const callback = jest.fn();
-		const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
-		const link = wrapper.find('.status-link');
-		link.simulate('click', {
-			preventDefault() {}
-		});
-		expect(callback).toHaveBeenCalled();
-	});
+  it('Should call onGenerateAuralUpdate when state-of-game link is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
+    const link = wrapper.find('.status-link');
+    link.simulate('click', {
+      preventDefault() {}
+    });
+    expect(callback).toHaveBeenCalled();
+  });
 });
